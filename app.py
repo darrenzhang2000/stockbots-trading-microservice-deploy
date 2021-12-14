ec1293b1f04d3b678fe09aaebf319d5ad50b81ae
Darren <darrenzhang200027@gmail.com>
2021-12-13 22:05:14 -0500

    new requiremtsn

--- app.py
+++ app.py
@@ -22,16 +22,6 @@ def getPortfolios():
     return portfolios
 
 def runAlgoJob():
-    url = f"{BACKEND_API}/users/register"
-
-    payload='email=aaaa%40gmail.com&password=aaa&firstName=a&lastName=a'
-    headers = {
-    'Content-Type': 'application/x-www-form-urlencoded'
-    }
-
-    response = requests.request("POST", url, headers=headers, data=payload)
-
-    print(response.text)
     time.sleep(86400)
     
     portfolios = getPortfolios()
